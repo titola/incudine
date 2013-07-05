3dabeedd38414a79cf693d2b0692f71b42d4f11f
Tito Latini <tito.01beta@gmail.com>
2013-07-05 16:15:00 +0200

    moogladder: transistor thermal voltage is a constant

--- src/vug/moog-vcf.lisp
+++ src/vug/moog-vcf.lisp
@@ -20,6 +20,11 @@
 ;;; Implementation of the Moog Ladder Filter", DAFx'04, Naples, Italy.
 ;;;
 ;;; Based on Victor Lazzarini's moogladder opcode (Csound)
+
+(define-constant +r-moogladder-thermal+ 40000.0d0)
+;;; Transistor thermal voltage
+(define-constant +moogladder-thermal+ (/ 1.0d0 +r-moogladder-thermal+))
+
 (define-vug moogladder (in freq resonance)
   (with-samples (x0 y0 y1 stage0 stage1 stage2 stage3 tanh-0 tanh-1 tanh-2)
     (with-samples ((f (* freq *sample-duration*))
@@ -29,11 +34,9 @@
                    (freq-correction (- (+ (* 1.8730 fff) (* 0.4955 ff) 0.9988)
                                        (* 0.6490 f)))
                    (amp-correction (+ (* -3.9364 ff) (* 1.8409 f) 0.9968))
-                   ;; Transistor thermal voltage
-                   (thermal (/ 1.0d0 40000.0d0))
                    ;; Filter tuning
                    (tune (* (- 1.0d0 (exp (- (* +twopi+ f/2 freq-correction))))
-                            40000.0d0))
+                            +r-moogladder-thermal+))
                    (res4 (if (plusp resonance)
                              (* 4.0d0 resonance amp-correction)
                              0.0d0)))
@@ -41,13 +44,13 @@
                    `(progn ,@(loop repeat n append body))))
         (with-oversampling (2)
           (setf x0 (- in (* res4 y0)))
-          (incf stage0 (* tune (- (tanh (* x0 thermal)) tanh-0)))
-          (setf tanh-0 (tanh (* stage0 thermal)))
+          (incf stage0 (* tune (- (tanh (* x0 +moogladder-thermal+)) tanh-0)))
+          (setf tanh-0 (tanh (* stage0 +moogladder-thermal+)))
           (incf stage1 (* tune (- tanh-0 tanh-1)))
-          (setf tanh-1 (tanh (* stage1 thermal)))
+          (setf tanh-1 (tanh (* stage1 +moogladder-thermal+)))
           (incf stage2 (* tune (- tanh-1 tanh-2)))
-          (setf tanh-2 (tanh (* stage2 thermal)))
-          (incf stage3 (* tune (- tanh-1 (tanh (* stage3 thermal)))))
+          (setf tanh-2 (tanh (* stage2 +moogladder-thermal+)))
+          (incf stage3 (* tune (- tanh-1 (tanh (* stage3 +moogladder-thermal+)))))
           ;; 1/2-sample delay for phase compensation
           (setf y0 (* (+ stage3 y1) 0.5))
           (setf y1 stage3))
