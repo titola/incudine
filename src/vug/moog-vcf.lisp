548f6f6cfca42e01afb0b8d20b6519c564e59225
Tito Latini <tito.01beta@gmail.com>
2013-07-06 18:28:48 +0200

    another Moog VCF

--- src/vug/moog-vcf.lisp
+++ src/vug/moog-vcf.lisp
@@ -55,3 +55,37 @@
           (setf y0 (* (+ stage3 y1) 0.5))
           (setf y1 stage3))
         y0))))
+
+;;; A discrete-time version of the Moog VCF; see Federico Fontana,
+;;; "Preserving the structure of the Moog VCF in the digital domain",
+;;; ICMC07, Copenhagen.
+(define-vug moogff (in freq resonance (reset-p boolean))
+  (with-samples (y s s1 s2 s3 s4 u past future)
+    (with ((wc (* 2.0d0 (tan (* pi freq *sample-duration*))
+                  *sample-rate*))
+           (wct (if (plusp wc) (* *sample-duration* wc) 0.0d0))
+           (b0 (/ wct (+ wct 2.0d0)))
+           (b0b0 (* b0 b0))
+           (b0b0b0b0 (* b0b0 b0b0))
+           (a1 (/ (- wct 2.0d0) (+ wct 2.0d0)))
+           (reset (when reset-p
+                    (setf s1 0.0d0 s2 0.0d0 s3 0.0d0 s4 0.0d0
+                          reset-p nil))))
+      (declare (type sample wc wct b0 b0b0 b0b0b0b0 a1))
+      ;; No warning
+      (initialize reset)
+      (setf s (+ s4 (* b0 (+ s3 (* b0 (+ s2 (* b0 s1)))))))
+      (setf y (* (+ (* b0b0b0b0 in) s)
+                 (/ 1.0d0 (+ 1.0d0 (* b0b0b0b0 resonance)))))
+      (setf u (- in (* resonance y)))
+      (setf past u)
+      (setf future (+ (* b0 past) s1))
+      (setf s1 (- (* b0 past) (* a1 future)))
+      (setf past future)
+      (setf future (+ (* b0 past) s2))
+      (setf s2 (- (* b0 past) (* a1 future)))
+      (setf past future)
+      (setf future (+ (* b0 past) s3))
+      (setf s3 (- (* b0 past) (* a1 future)))
+      (setf s4 (- (* b0 future) (* a1 y)))
+      y)))
