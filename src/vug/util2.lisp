titola/incudine

commit 154d1837b285ed78f9090e2dc6244a56a383932e

fix coercing in CLIP

src/vug/util2.lisp
@@ -165,7 +165,7 @@
            (wrap-cond (,in ,lo ,hi ,delta ,(or offset in))
              ((>= ,in ,hi) (decf ,in ,delta))
              (,(if range `(< ,in ,lo) `(minusp ,in))
-               (incf ,in ,delta)))
+              (incf ,in ,delta)))
            ,in))))
 
   (defmacro %mirror-consequent (in threshold1 threshold2 range
@@ -200,9 +200,12 @@
 
 (declaim (inline clip))
 (defun clip (in low high)
-  (cond ((> in high) high)
-        ((< in low) low)
-        (t in)))
+  (declare (type sample in) (type real low high))
+  (let ((low (sample low))
+        (high (sample high)))
+    (cond ((> in high) high)
+          ((< in low) low)
+          (t in))))
 
 (define-vug wrap (in low high)
   (with-samples ((range (- high low))

commit 8e00f4e74ec64b479db7fe86b6b68df3aa3c5c25

CLIP works also with non SAMPLE type
fix a bug in BUF-VDELAY and VTAP caused by a recent change (154d183)

src/vug/util2.lisp
@@ -200,12 +200,13 @@
 
 (declaim (inline clip))
 (defun clip (in low high)
-  (declare (type sample in) (type real low high))
-  (let ((low (sample low))
-        (high (sample high)))
-    (cond ((> in high) high)
-          ((< in low) low)
-          (t in))))
+  (flet ((%clip (in low high)
+           (cond ((> in high) high)
+                 ((< in low) low)
+                 (t in))))
+    (if (typep in 'sample)
+        (%clip in (sample low) (sample high))
+        (%clip in low high))))
 
 (define-vug wrap (in low high)
   (with-samples ((range (- high low))