titola/incudine

commit b7a69d2d6ad8842ca8a64d918e793bee8f9aac7b

SMP-REF replaces DATA-REF (deprecated)
Simplification of the code by using SMP-REF where it is opportune.

src/analysis/base.lisp
@@ -20,7 +20,7 @@
 
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (import
-   '(incudine:data-ref
+   '(incudine:smp-ref
      incudine:free
      incudine:now)))
 
@@ -127,10 +127,8 @@
 (declaim (inline ring-input-buffer-put))
 (defun ring-input-buffer-put (value buf)
   (declare (type sample value) (type ring-input-buffer buf))
-  (setf (mem-ref (ring-input-buffer-data buf) 'sample
-                 (the non-negative-fixnum
-                   (* (ring-input-buffer-head buf)
-                      +foreign-sample-size+)))
+  (setf (smp-ref (ring-input-buffer-data buf)
+                 (ring-input-buffer-head buf))
         value)
   (incf (ring-buffer-head buf))
   (when (>= (ring-buffer-head buf)
@@ -158,19 +156,16 @@
 (declaim (inline ring-output-buffer-next))
 (defun ring-output-buffer-next (buf)
   (declare (type ring-output-buffer buf))
-  (let ((index (the non-negative-fixnum
-                 (* (ring-output-buffer-head buf)
-                    +foreign-sample-size+))))
-    ;; Temporary variable on the foreign heap
-    (setf (mem-ref (ring-output-buffer-tmp buf) 'sample)
-          #1=(mem-ref (ring-output-buffer-data buf)
-                      'sample index))
-    (setf #1# +sample-zero+)
-    (incf (ring-output-buffer-head buf))
-    (when (>= (ring-output-buffer-head buf)
-              (ring-output-buffer-size buf))
-      (setf (ring-output-buffer-head buf) 0))
-    (mem-ref (ring-output-buffer-tmp buf) 'sample)))
+  ;; Temporary variable on the foreign heap
+  (setf (smp-ref (ring-output-buffer-tmp buf) 0)
+        #1=(smp-ref (ring-output-buffer-data buf)
+                    (ring-output-buffer-head buf)))
+  (setf #1# +sample-zero+)
+  (incf (ring-output-buffer-head buf))
+  (when (>= (ring-output-buffer-head buf)
+            (ring-output-buffer-size buf))
+    (setf (ring-output-buffer-head buf) 0))
+  (smp-ref (ring-output-buffer-tmp buf) 0))
 
 (defstruct (analysis (:copier nil))
   (input-buffer (error "missing INPUT-BUFFER") :type foreign-pointer)
@@ -184,12 +179,12 @@
 (declaim (inline analysis-time))
 (defun analysis-time (obj)
   (declare (type analysis obj))
-  (mem-ref (analysis-time-ptr obj) 'sample))
+  (smp-ref (analysis-time-ptr obj) 0))
 
 (declaim (inline set-analysis-time))
 (defun set-analysis-time (obj time)
   (declare (type analysis obj) (type sample time))
-  (setf (mem-ref (analysis-time-ptr obj) 'sample) time))
+  (setf (smp-ref (analysis-time-ptr obj) 0) time))
 
 (defsetf analysis-time set-analysis-time)
 
@@ -225,7 +220,7 @@
                             analysis-object)))
       (let ((time-ptr (foreign-alloc 1 nil))
             (data (foreign-alloc size t)))
-        (setf (mem-ref time-ptr 'sample) (sample -1))
+        (setf (smp-ref time-ptr 0) (sample -1))
         (let ((obj (%make-abuffer :data data
                       :size size
                       :nbins nbins
@@ -270,19 +265,13 @@
     (setf (abuffer-coord-complex-p obj) t)))
 
 (defmacro abuffer-time (obj)
-  `(mem-ref (abuffer-time-ptr ,obj) 'sample))
+  `(smp-ref (abuffer-time-ptr ,obj) 0))
 
 (defmacro abuffer-realpart (obj nbin)
-  `(mem-ref (abuffer-data ,obj) 'sample
-            (the non-negative-fixnum
-              (* ,nbin +foreign-complex-size+))))
+  `(smp-ref (abuffer-data ,obj) ,nbin))
 
 (defmacro abuffer-imagpart (obj nbin)
-  `(mem-ref (abuffer-data ,obj) 'sample
-            (the non-negative-fixnum
-              (+ (the non-negative-fixnum
-                   (* ,nbin +foreign-complex-size+))
-                 +foreign-sample-size+))))
+  `(smp-ref (abuffer-data ,obj) (1+ ,nbin)))
 
 (defgeneric compute (obj &optional arg))
 

src/analysis/fft.lisp
@@ -172,7 +172,7 @@ is the plan for a IFFT."
   (declare (type foreign-pointer c-array)
            (type non-negative-fixnum size))
   (dotimes (i size c-array)
-    (setf (data-ref c-array i) #.(sample 1))))
+    (setf (smp-ref c-array i) #.(sample 1))))
 
 (declaim (inline fill-window-buffer))
 (defun fill-window-buffer (buffer function size)
@@ -254,10 +254,8 @@ is the plan for a IFFT."
 (declaim (inline fft-input))
 (defun fft-input (fft)
   (let ((buf (fft-ring-buffer fft)))
-    (mem-ref (ring-input-buffer-data buf) 'sample
-             (the non-negative-fixnum
-               (* (ring-input-buffer-head buf)
-                  +foreign-sample-size+)))))
+    (smp-ref (ring-input-buffer-data buf)
+             (ring-input-buffer-head buf))))
 
 (declaim (inline set-fft-input))
 (defun set-fft-input (fft input)

src/analysis/pvbuffer.lisp
@@ -74,21 +74,21 @@
     `(progn
        ;; Fill the first half of the buffer (the FFT size is 2*partsize)
        (dotimes (,index ,partsize)
-         (setf (data-ref ,fft-inbuf ,index)
+         (setf (smp-ref ,fft-inbuf ,index)
                (if (< ,bufpos ,bufsize)
-                   (data-ref ,bufdata ,bufpos)
+                   (smp-ref ,bufdata ,bufpos)
                    +sample-zero+))
          (incf ,bufpos ,channels))
        ;; Zero-padding the second half of the buffer
        (loop for ,index from ,partsize below ,fft-size do
-            (setf (data-ref ,fft-inbuf ,index) +sample-zero+)))))
+            (setf (smp-ref ,fft-inbuf ,index) +sample-zero+)))))
 
 (defmacro fft-output-to-pvbuffer-frame (fft-outbuf pvbuf-data pvbuf-pos
                                         block-size)
   (with-gensyms (index)
     `(dotimes (,index ,block-size)
-       (setf (data-ref ,pvbuf-data ,pvbuf-pos)
-             (data-ref ,fft-outbuf ,index))
+       (setf (smp-ref ,pvbuf-data ,pvbuf-pos)
+             (smp-ref ,fft-outbuf ,index))
        (incf ,pvbuf-pos))))
 
 (defun buffer->pvbuffer (buf partsize &key (start 0) (frames 0))

src/buffer.lisp
@@ -90,13 +90,13 @@
 (declaim (inline buffer-value))
 (defun buffer-value (buffer index)
   (declare (type buffer buffer) (type non-negative-fixnum index))
-  (data-ref (buffer-data buffer) index))
+  (smp-ref (buffer-data buffer) index))
 
 (declaim (inline set-buffer-value))
 (defun set-buffer-value (buffer index value)
   (declare (type buffer buffer) (type non-negative-fixnum index)
            (type real value))
-  (setf (data-ref (buffer-data buffer) index)
+  (setf (smp-ref (buffer-data buffer) index)
         (sample value)))
 
 (defsetf buffer-value set-buffer-value)
@@ -239,7 +239,7 @@ It is possible to use line comments that begin with the `;' char."
 (defun save-data-to-textfile (data path size)
   (with-open-file (f path :direction :output :if-exists :supersede)
     (dotimes (i size)
-      (write-line (incudine.util::sample->string (data-ref data i)) f))))
+      (write-line (incudine.util::sample->string (smp-ref data i)) f))))
 
 (defun buffer-save (buf path &key (start 0) (end 0) sample-rate
                     textfile-p (header-type *default-header-type*)
@@ -318,8 +318,8 @@ It is possible to use line comments that begin with the `;' char."
                (if (= index size)
                    max
                    (norm (1+ index)
-                         (max (data-ref data index) max)))))
-      (scale obj (/ norm-value (norm 1 (data-ref data 0)))))))
+                         (max (smp-ref data index) max)))))
+      (scale obj (/ norm-value (norm 1 (smp-ref data 0)))))))
 
 (defmethod rescale ((obj buffer) (min real) (max real))
   (let ((data (buffer-data obj))
@@ -331,12 +331,12 @@ It is possible to use line comments that begin with the `;' char."
                (if (= index size)
                    (values (/ (sample 1) (- old-max old-min))
                            old-min)
-                   (let ((value (data-ref data index)))
+                   (let ((value (smp-ref data index)))
                      (resc (1+ index)
                            (min value old-min)
                            (max value old-max))))))
       (multiple-value-bind (old-delta old-min)
-          (resc 1 (data-ref data 0) (data-ref data 0))
+          (resc 1 (smp-ref data 0) (smp-ref data 0))
         (map-buffer (lambda (index value)
                       (declare (ignore index))
                       (+ min (* (- max min) old-delta (- value old-min))))

src/bus.lisp
@@ -66,39 +66,39 @@
 (declaim (inline bus))
 (defun bus (num)
   (declare (type bus-number num))
-  (data-ref *bus-pointer* num))
+  (smp-ref *bus-pointer* num))
 
 (declaim (inline set-bus))
 (defun set-bus (num value)
   (declare (type bus-number num))
-  (setf (data-ref *bus-pointer* num) (sample value)))
+  (setf (smp-ref *bus-pointer* num) (sample value)))
 
 (defsetf bus set-bus)
 
 (declaim (inline audio-in))
 (defun audio-in (channel)
   (declare (type channel-number channel))
-  (data-ref *input-pointer* channel))
+  (smp-ref *input-pointer* channel))
 
 (declaim (inline audio-out))
 (defun audio-out (channel)
   (declare (type channel-number channel))
-  (data-ref *output-pointer* channel))
+  (smp-ref *output-pointer* channel))
 
 (declaim (inline set-audio-out))
 (defun set-audio-out (channel value)
   (declare (type channel-number channel))
-  (setf (data-ref *output-pointer* channel) (sample value)))
+  (setf (smp-ref *output-pointer* channel) (sample value)))
 
 (defsetf audio-out set-audio-out)
 
 (defun update-peak-values (chan)
   (declare #.*standard-optimize-settings*
            (type channel-number chan))
-  (let ((value (mem-aref incudine::*output-pointer* 'sample chan)))
+  (let ((value (smp-ref incudine::*output-pointer* chan)))
     (declare (type sample value))
-    (when (> value (mem-aref *output-peak-values* 'sample chan))
-      (setf (mem-aref *output-peak-values* 'sample chan) value))
+    (when (> value (smp-ref *output-peak-values* chan))
+      (setf (smp-ref *output-peak-values* chan) value))
     (when (> value (sample 1))
       (setf #1=(svref *out-of-range-counter* chan)
             (the positive-fixnum
@@ -108,14 +108,14 @@
 (declaim (inline peak-info))
 (defun peak-info (chan)
   (declare (type channel-number chan))
-  (values (data-ref *output-peak-values* chan)
+  (values (smp-ref *output-peak-values* chan)
           (svref *out-of-range-counter* chan)))
 
 (defun print-peak-info (&optional (channels *number-of-output-bus-channels*)
                         (stream *standard-output*))
   (format stream "~11tpeak amps:  ")
   (foreach-channel (ch channels)
-    (format stream "~8,3,F  " (data-ref *output-peak-values* ch)))
+    (format stream "~8,3,F  " (smp-ref *output-peak-values* ch)))
   (format stream "~%samples out of range:  ")
   (foreach-channel (ch channels)
     (format stream "~8,,D  " (svref *out-of-range-counter* ch)))
@@ -124,7 +124,7 @@
 (declaim (inline %reset-peak-values))
 (defun %reset-peak-meters ()
   (foreach-channel (chan *number-of-output-bus-channels*)
-    (setf (data-ref *output-peak-values* chan) +sample-zero+)
+    (setf (smp-ref *output-peak-values* chan) +sample-zero+)
     (setf (svref *out-of-range-counter* chan) 0)))
 
 (defun reset-peak-meters ()

src/deprecated.lisp
@@ -10,6 +10,13 @@
   (incudine::deprecated-msg 'synth-seq 'dsp-seq)
   `(dsp-seq ,@functions))
 
+(in-package :incudine.util)
+
+;;; 20130927
+(defmacro data-ref (data index)
+  (incudine::deprecated-msg 'data-ref 'smp-ref)
+  `(smp-ref ,data ,index))
+
 (in-package :incudine.vug)
 
 ;;; 20130904

src/envelope.lisp
@@ -120,7 +120,7 @@
                      ((atom curve) (list curve))
                      (t curve)))
         (first-level (sample (car levels))))
-    (setf (data-ref data 0)
+    (setf (smp-ref data 0)
           (envelope-fix-zero first-level (car curve)))
     (setf (envelope-duration env) +sample-zero+
           (envelope-loop-node env) loop-node
@@ -133,10 +133,10 @@
       (declare (type positive-fixnum i)
                (type list lev tim cur))
       (incf (envelope-duration env)
-            (setf (data-ref data i) (sample (car tim))))
-      (setf (data-ref data (incf i))
+            (setf (smp-ref data i) (sample (car tim))))
+      (setf (smp-ref data (incf i))
             (envelope-fix-zero (car lev) (car cur)))
-      (setf (data-ref data (incf i))
+      (setf (smp-ref data (incf i))
             (seg-function-spec->sample (car cur))))
     env))
 
@@ -287,11 +287,11 @@
            (type non-negative-fixnum node-number)
            #.*reduce-warnings*)
   (when (check-envelope-node env node-number)
-    (data-ref (envelope-data env)
-              (if (zerop node-number)
-                  0
-                  (the non-negative-fixnum
-                    (1- (* node-number 3)))))))
+    (smp-ref (envelope-data env)
+             (if (zerop node-number)
+                 0
+                 (the non-negative-fixnum
+                   (1- (* node-number 3)))))))
 
 (declaim (inline check-envelope-node))
 (defun check-envelope-node (env number)
@@ -315,19 +315,19 @@
         (block nil
           (when (and (plusp node-number)
                      (exponential-curve-index-p
-                      (mem-ref data 'sample)))
+                      (smp-ref data 0)))
             (return (setf level (sample 1.0e-5))))
           (when (/= node-number (1- (the non-negative-fixnum
                                       (envelope-points env))))
             (incf-pointer data (* 3 +foreign-sample-size+))
             (when (exponential-curve-index-p
-                   (mem-ref data 'sample))
+                   (smp-ref data 0))
               (incf-pointer data (* -3 +foreign-sample-size+))
               (return (setf level (sample 1.0e-5))))
             (incf-pointer data (* -3 +foreign-sample-size+)))))
       (if (> node-number 0)
           (incf-pointer data (- +foreign-sample-size+)))
-      (setf (mem-ref data 'sample) (sample level)))))
+      (setf (smp-ref data 0) (sample level)))))
 
 (defsetf envelope-level set-envelope-level)
 
@@ -340,9 +340,9 @@
   (when (check-envelope-node env node-number)
     (if (zerop node-number)
         +sample-zero+
-        (data-ref (envelope-data env)
-                  (the non-negative-fixnum
-                    (- (* node-number 3) 2))))))
+        (smp-ref (envelope-data env)
+                 (the non-negative-fixnum
+                   (- (* node-number 3) 2))))))
 
 (declaim (inline set-envelope-time))
 (defun set-envelope-time (env node-number time)
@@ -353,9 +353,9 @@
            #.*reduce-warnings*)
   (cond ((zerop node-number) +sample-zero+)
         ((< node-number (the non-negative-fixnum (envelope-points env)))
-         (setf (data-ref (envelope-data env)
-                         (the positive-fixnum
-                           (- (* node-number 3) 2)))
+         (setf (smp-ref (envelope-data env)
+                        (the positive-fixnum
+                          (- (* node-number 3) 2)))
                (sample time)))
         (t +sample-zero+)))
 
@@ -371,9 +371,9 @@
   (when (< 0 node-number (the non-negative-fixnum
                            (envelope-points env)))
     (sample->seg-function-spec
-     (data-ref (envelope-data env)
-               (the non-negative-fixnum
-                 (* 3 node-number))))))
+     (smp-ref (envelope-data env)
+              (the non-negative-fixnum
+                (* 3 node-number))))))
 
 (defun set-envelope-curve (env node-number curve)
   (declare #.*standard-optimize-settings*
@@ -383,8 +383,8 @@
            #.*reduce-warnings*
            #+(or cmu sbcl) (values (or symbol sample)))
   (macrolet ((segment-fix-zero (ptr new-zero old-zero)
-               `(when (= (data-ref ,ptr 0) ,old-zero)
-                  (setf (data-ref ,ptr 0) ,new-zero)))
+               `(when (= (smp-ref ,ptr 0) ,old-zero)
+                  (setf (smp-ref ,ptr 0) ,new-zero)))
              (segment-fix-zeros (beg-ptr end-ptr new-zero old-zero)
                `(progn
                   (segment-fix-zero ,beg-ptr ,new-zero ,old-zero)
@@ -402,7 +402,7 @@
             (segment-fix-zeros beg-ptr end-ptr (sample 1.0e-5) 0)
             (segment-fix-zeros beg-ptr end-ptr +sample-zero+
                                (sample 1.0e-5)))
-        (setf (data-ref curve-ptr 0)
+        (setf (smp-ref curve-ptr 0)
               (seg-function-spec->sample curve))
         curve))))
 
@@ -415,10 +415,10 @@
   (let ((points (envelope-points obj))
         (data (envelope-data obj)))
     (setf (envelope-level obj 0)
-          (* (data-ref data 0) mult))
+          (* (smp-ref data 0) mult))
     (loop for i from 1 below points do
          (setf (envelope-level obj i)
-               (* (data-ref data (1- (* i 3))) mult)))
+               (* (smp-ref data (1- (* i 3))) mult)))
     obj))
 
 (defmethod normalize ((obj envelope) (norm-value real))
@@ -445,7 +445,7 @@
                (if (>= index points)
                    (values (/ (sample 1) (- old-max old-min))
                            old-min)
-                   (let ((value (data-ref (envelope-data obj) index)))
+                   (let ((value (smp-ref (envelope-data obj) index)))
                      (resc (+ index 3)
                            (min value old-min)
                            (max value old-max))))))
@@ -456,8 +456,8 @@
           (flet ((set-level (index offset)
                    (setf (envelope-level obj index)
                          (+ min (* new-delta old-delta
-                                   (- (data-ref (envelope-data obj)
-                                                offset)
+                                   (- (smp-ref (envelope-data obj)
+                                               offset)
                                       old-min))))))
             (set-level 0 0)
             (labels ((set-levels (index offset)

src/gen/envelope.lisp
@@ -43,7 +43,7 @@
            (remain 0))
       (declare (type non-negative-fixnum size size-remained data-size
                      guard-point index dur remain))
-      (with-samples* ((level (data-ref env-data 0))
+      (with-samples* ((level (smp-ref env-data 0))
                       (time-scale (/ size (envelope-duration env)))
                       (end level)
                       (max level)
@@ -54,7 +54,7 @@
                       (y2 0.0)
                       (curve incudine:+seg-lin-func+))
         (dotimes (i size)
-          (setf (mem-aref c-array 'sample i)
+          (setf (smp-ref c-array i)
                 (if (zerop remain)
                     (cond ((>= (incf index) data-size)
                            (nrt-msg error "the envelope ~A is corrupted" env)
@@ -62,15 +62,15 @@
                           (t (if (= index guard-point)
                                  (setf dur (max 1 size-remained))
                                  (setf dur (max 1 (sample->fixnum
-                                                   (* (data-ref env-data index)
+                                                   (* (smp-ref env-data index)
                                                       time-scale)))
                                        size-remained (- size-remained dur)))
                              (setf remain (1- dur)
                                    index (1+ index)
                                    level end
-                                   end (data-ref env-data index)
+                                   end (smp-ref env-data index)
                                    index (1+ index)
-                                   curve (data-ref env-data index))
+                                   curve (smp-ref env-data index))
                              (when (> end max) (setf max end))
                              (%segment-init level end dur curve grow a2 b1 y1 y2)
                              level))
@@ -78,7 +78,7 @@
                            (%segment-update-level level curve grow a2 b1 y1 y2)
                            level))))
         (unless periodic-p
-          (setf (mem-aref c-array 'sample size) end))
+          (setf (smp-ref c-array size) end))
         (values c-array
                 ;; Factor to scale the amplitude
                 (/ max)

commit 52f2703f4914f4b1ad0d117db9d90a3d44210c44

fix ABUFFER-{REAL,IMAG}PART
bug introduced in a recent commit (b7a69d2)

src/analysis/base.lisp
@@ -268,10 +268,16 @@
   `(smp-ref (abuffer-time-ptr ,obj) 0))
 
 (defmacro abuffer-realpart (obj nbin)
-  `(smp-ref (abuffer-data ,obj) ,nbin))
+  `(mem-ref (abuffer-data ,obj) 'sample
+            (the non-negative-fixnum
+              (* ,nbin +foreign-complex-size+))))
 
 (defmacro abuffer-imagpart (obj nbin)
-  `(smp-ref (abuffer-data ,obj) (1+ ,nbin)))
+  `(mem-ref (abuffer-data ,obj) 'sample
+            (the non-negative-fixnum
+              (+ (the non-negative-fixnum
+                   (* ,nbin +foreign-complex-size+))
+                 +foreign-sample-size+))))
 
 (defgeneric compute (obj &optional arg))
 