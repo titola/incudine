3e720bc793eab83e75f46eb0c98389d4e26f7007
Tito Latini <tito.01beta@gmail.com>
2015-07-03 22:33:02 +0200

    Tidying of MOOGFF VUG

--- src/vug/moog-vcf.lisp
+++ src/vug/moog-vcf.lisp
@@ -1,4 +1,4 @@
-;;; Copyright (c) 2013-2014 Tito Latini
+;;; Copyright (c) 2013-2015 Tito Latini
 ;;;
 ;;; This program is free software; you can redistribute it and/or modify
 ;;; it under the terms of the GNU General Public License as published by
@@ -16,16 +16,21 @@
 
 (in-package :incudine.vug)
 
-;;; Moog Ladder Filter; see Antti Huovilainen, "Non-Linear Digital
-;;; Implementation of the Moog Ladder Filter", DAFx'04, Naples, Italy.
+;;; Moog Ladder Filter.
+;;;
+;;; Reference:
+;;;
+;;;   [1] Antti Huovilainen, "Non-Linear Digital Implementation of the
+;;;   Moog Ladder Filter", DAFx'04, Naples, Italy.
 ;;;
 ;;; Based on Victor Lazzarini's moogladder opcode (Csound)
-
+;;;
 (define-constant +r-moogladder-thermal+ (sample 40000))
 ;;; Transistor thermal voltage
 (define-constant +moogladder-thermal+ (/ +r-moogladder-thermal+))
 
 (define-vug moogladder (in freq resonance)
+  "Non linear digital implementation of the Moog ladder filter."
   (with-samples (x0 y0 y1 stage0 stage1 stage2 stage3 tanh-0 tanh-1 tanh-2)
     (with-samples ((f (* freq *sample-duration*))
                    (ff (* f f))
@@ -57,35 +62,35 @@
           (setf y1 stage3))
         y0))))
 
-;;; A discrete-time version of the Moog VCF; see Federico Fontana,
-;;; "Preserving the structure of the Moog VCF in the digital domain",
-;;; ICMC07, Copenhagen.
+;;; A discrete-time version of the Moog VCF.
+;;;
+;;; Reference:
+;;;
+;;;   [1] Federico Fontana, "Preserving the structure of the Moog VCF
+;;;   in the digital domain", ICMC07, Copenhagen.
+;;;
+(defmacro moogff-update-states (in out &rest states)
+  (with-gensyms (scaled-past future)
+    `(with-samples (,scaled-past ,future)
+       ,@(loop for s in (butlast states)
+               for past = in then future
+               append `((setf ,scaled-past (* b0 ,past))
+                        (setf ,future (+ ,scaled-past ,s))
+                        (setf ,s (- ,scaled-past (* a1 ,future)))))
+       (setf ,@(last states) (- (* b0 ,future) (* a1 ,out))))))
+
 (define-vug moogff (in freq resonance (reset-p boolean))
-  (with-samples (y s s1 s2 s3 s4 u past future)
-    (with ((wc (* 2.0 *sample-rate* (tan (* freq *pi-div-sr*))))
-           (wct (if (plusp wc) (* *sample-duration* wc) +sample-zero+))
-           (b0 (/ wct (+ wct 2.0)))
-           (b0b0 (* b0 b0))
-           (b0b0b0b0 (* b0b0 b0b0))
-           (a1 (/ (- wct 2.0) (+ wct 2.0)))
-           (reset (when reset-p
-                    (samples-zero s1 s2 s3 s4)
-                    (setf reset-p nil))))
-      (declare (type sample wc wct b0 b0b0 b0b0b0b0 a1))
-      ;; No warning
-      (initialize reset)
+  "A discrete-time version of the Moog VCF."
+  (with-samples (y s s1 s2 s3 s4)
+    (with-samples ((%wct (* 2 (tan (* freq *pi-div-sr*))))
+                   (wct (if (plusp %wct) %wct +sample-zero+))
+                   (a1 (/ (- wct 2) (+ wct 2)))
+                   (b0 (/ wct (+ wct 2)))
+                   (b0b0 (* b0 b0))
+                   (b (* b0b0 b0b0)))
+      (with-follow (reset-p)
+        (if reset-p (samples-zero s1 s2 s3 s4)))
       (setf s (+ s4 (* b0 (+ s3 (* b0 (+ s2 (* b0 s1)))))))
-      (setf y (* (+ (* b0b0b0b0 in) s)
-                 (/ 1.0 (+ 1.0 (* b0b0b0b0 resonance)))))
-      (setf u (- in (* resonance y)))
-      (setf past u)
-      (setf future (+ (* b0 past) s1))
-      (setf s1 (- (* b0 past) (* a1 future)))
-      (setf past future)
-      (setf future (+ (* b0 past) s2))
-      (setf s2 (- (* b0 past) (* a1 future)))
-      (setf past future)
-      (setf future (+ (* b0 past) s3))
-      (setf s3 (- (* b0 past) (* a1 future)))
-      (setf s4 (- (* b0 future) (* a1 y)))
+      (setf y (/ (+ (* b in) s) (+ 1.0 (* b resonance))))
+      (moogff-update-states (- in (* resonance y)) y s1 s2 s3 s4)
       y)))
