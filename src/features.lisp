dd0b6d5cb65a070816db4f02ff58c68fbc06caa0
Tito Latini <tito.01beta@gmail.com>
2016-06-18 13:55:12 +0200

    Fix loading incudine with an empty ~/.incudinerc

--- src/features.lisp
+++ src/features.lisp
@@ -5,22 +5,25 @@
           (merge-pathnames "config-specials.lisp" *load-pathname*)))
 
 (let ((init-file (merge-pathnames ".incudinerc" (user-homedir-pathname))))
-  (cond ((probe-file init-file)
-         (load init-file)
-         (let ((jack-midi-p (and (boundp *enable-jack-midi*) *enable-jack-midi*)))
-           (if (boundp *audio-driver*)
-               (case *audio-driver*
-                 (:jack (when jack-midi-p
-                          (pushnew :jack-midi *features*))
-                        (pushnew :jack-audio *features*))
-                 ((:portaudio :portaudio-jack) (pushnew :portaudio *features*))
-                 (:dummy (pushnew :dummy-audio *features*)))
-               (when jack-midi-p
-                 (pushnew :jack-midi *features*)
-                 (pushnew :jack-audio *features*)))))
-        (t (pushnew #+linux :jack-audio
+  (flet ((default-features ()
+           (pushnew #+linux :jack-audio
                     #-linux :portaudio
-                    *features*))))
+                    *features*)))
+    (cond ((probe-file init-file)
+           (load init-file)
+           (let ((jack-midi-p (and (boundp '*enable-jack-midi*) *enable-jack-midi*)))
+             (cond ((boundp '*audio-driver*)
+                    (case *audio-driver*
+                      (:jack (when jack-midi-p
+                               (pushnew :jack-midi *features*))
+                             (pushnew :jack-audio *features*))
+                      ((:portaudio :portaudio-jack) (pushnew :portaudio *features*))
+                      (:dummy (pushnew :dummy-audio *features*))))
+                   (jack-midi-p
+                    (pushnew :jack-midi *features*)
+                    (pushnew :jack-audio *features*))
+                   (t (default-features)))))
+          (t (default-features)))))
 
 (in-package :cl-user)
 (delete-package "INCUDINE.FEATURES")
