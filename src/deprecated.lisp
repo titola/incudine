b9e84560e9f19fa0ddb764f5428b99441cbc44a8
Tito Latini <tito.01beta@gmail.com>
2018-02-23 11:16:07 +0100

    Add INCUDINE-DEPRECATION-WARNING and DEPRECATED-FUNCTION macro.

--- src/deprecated.lisp
+++ src/deprecated.lisp
@@ -1,16 +1,37 @@
 (in-package :incudine)
 
+(define-condition incudine-deprecation-warning (simple-warning) ())
+
+(defvar *deprecated* nil)
+
+(defmacro deprecated-function ((name args &rest body)
+                               &key run-time-message-p date package
+                               (replacement (car body)))
+  (with-gensyms (form rest)
+    `(progn
+       (export ',name ,(or package *package*))
+       (pushnew (list ',name :from ,date) *deprecated*)
+       (defun ,name ,args
+         ,@(if run-time-message-p
+               `((incudine::deprecated-msg ',name ',replacement)))
+         ,@body)
+       (define-compiler-macro ,name (&whole ,form &rest ,rest)
+         (declare (ignore ,rest))
+         (warn (make-condition 'incudine-deprecation-warning
+                 :format-control "~A is deprecated,~%use ~A instead."
+                 :format-arguments (list ',name ',replacement)))
+         ,form))))
+
 (defun deprecated-msg (old new)
-  (msg warn "~A is deprecated, use ~A instead." old new))
+  (msg warn "~A is deprecated,~%~6Tuse ~A instead." old new))
+
+;;;---------------------[ Deprecated symbols ]----------------------
 
 (in-package :incudine.util)
 
-(eval-when (:compile-toplevel :load-toplevel :execute)
-  (export
-   ;; 20180201
-   '(thread-set-priority)))
-
-(defun thread-set-priority (thread priority)
-  (incudine::deprecated-msg "THREAD-SET-PRIORITY"
-                            "(SETF (THREAD-PRIORITY THREAD) PRIO)")
-  (setf (thread-priority thread) priority))
+(incudine::deprecated-function
+   (thread-set-priority (thread priority)
+     (setf (thread-priority thread) priority))
+   :date 20180201
+   :package "INCUDINE.UTIL"
+   :run-time-message-p t)
