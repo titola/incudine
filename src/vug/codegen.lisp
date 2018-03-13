titola/incudine

commit e57a3b49f8aef97e7b2d3d7bba7e38b39151283f

Allow recursive DSP's.
FREE-HOOK used within the definition of a DSP works without workarounds.

Export NULL-NODE-P

src/vug/codegen.lisp
@@ -664,11 +664,11 @@
           (dolist (par #1# acc)
             (push (vug-parameter-fix-dependences par) acc)))))
 
-(defun dsp-vug-block (arguments &rest rest)
+(defun dsp-vug-block (name arguments &rest rest)
   (multiple-value-bind (args types) (arg-names-and-types arguments)
     `(reduce-vug-variables
        (with-vug-arguments (,args ,types)
-         (vug-block (with-argument-bindings (,args ,types) ,@rest))))))
+         (vug-block ,name (with-argument-bindings (,args ,types) ,@rest))))))
 
 (defmacro with-foreign-variables (specs &body body)
   (let ((ret body))
@@ -757,7 +757,7 @@
     `(let* ((*vug-variables* (make-vug-variables))
             (*variables-to-preserve* nil)
             (*initialization-code* (make-initialization-code-stack))
-            (,vug-body (format-vug-code ,(dsp-vug-block arguments obj)))
+            (,vug-body (format-vug-code ,(dsp-vug-block name arguments obj)))
             (,smpvec-size (add-sample-variables))
             (,f32vec-size (add-float-variables))
             (,f64vec-size (add-double-variables))
@@ -773,7 +773,7 @@
          `(lambda (%dsp-node%)
             (declare ,*standard-optimize-settings*
                      (type incudine:node %dsp-node%))
-            (with-dsp-preamble (,dsp ,',name ,control-table ,free-hook)
+            (with-dsp-preamble (,dsp ',',name ,control-table ,free-hook)
               (with-foreign-arrays ((,smpvec ,smpvecw 'sample ,,smpvec-size)
                                     (,f32vec ,f32vecw :float ,,f32vec-size)
                                     (,f64vec ,f64vecw :double ,,f64vec-size)
@@ -792,7 +792,7 @@
                        (set-controls-form control-table ',arg-names)
                        (reorder-initialization-code)
                        `(progn
-                          (setf (dsp-name ,dsp) ,',name)
+                          (setf (dsp-name ,dsp) ',',name)
                           (setf (node-controls %dsp-node%) ,control-table)
                           (update-free-hook %dsp-node% ,free-hook)
                           ,@(initialization-code)
@@ -1146,10 +1146,21 @@
                     (equal (incudine:control-names ,node) ',arg-names))
            (apply #',name (build-control-list ,node :replace ,node)))))))
 
-(defmacro reuse-dsp-instance (dsp node arg-names)
+(defmacro with-reserved-node ((node id) &body body)
+  `(let ((last-node-id incudine::*last-node-id*))
+     (unwind-protect
+          (progn
+            (setf (incudine:node-id ,node) ,id)
+            (setf incudine::*last-node-id* ,id)
+            ,@body)
+       (setf (incudine:node-id ,node) nil)
+       (setf incudine::*last-node-id* last-node-id))))
+
+(defmacro reuse-dsp-instance (dsp node id arg-names)
   (with-gensyms (obj)
     `(let ((,obj (car ,dsp)))
-       (funcall (dsp-init-function ,obj) ,node ,@arg-names)
+       (with-reserved-node (,node ,id)
+         (funcall (dsp-init-function ,obj) ,node ,@arg-names))
        (lambda (,node)
          (declare (ignore ,node))
          (values (dsp-init-function ,obj)
@@ -1186,7 +1197,7 @@
                (incudine::enqueue-node-function
                  (update-node-hooks ,node ,stop-hook ,free-hook)
                  (if ,dsp
-                     (reuse-dsp-instance ,dsp ,node ,arg-names)
+                     (reuse-dsp-instance ,dsp ,node ,node-id ,arg-names)
                      (,get-dsp-func ,@arg-names))
                  (dsp-init-args ,arg-bindings ,arg-names)
                  ,node-id ',name ,add-action ,target ,action ,fade-time
@@ -1227,7 +1238,7 @@
         (check-default-args args defaults 'dsp)
         `(macrolet ((,get-function ,arg-names
                       `(prog1
-                           ,(generate-dsp-code ',name ,args ,arg-names
+                           ,(generate-dsp-code ,name ,args ,arg-names
                                                (progn ,@form))
                          (nrt-msg info "new alloc for DSP ~A" ',',name))))
            (cond ((vug ',name)

incudine-tests.asd
@@ -2,7 +2,7 @@
 ;;;
 ;;; ASDF system definition for INCUDINE unit tests.
 ;;;
-;;; Copyright (c) 2013-2017 Tito Latini
+;;; Copyright (c) 2013-2018 Tito Latini
 ;;;
 ;;; This program is free software; you can redistribute it and/or modify
 ;;; it under the terms of the GNU General Public License as published by
@@ -67,6 +67,8 @@
      (:file "vug/local-func-over-vug" :depends-on ("vug/base"))
      (:file "vug/shadow-local-functions" :depends-on ("vug/base"))
      (:file "vug/ugen-reinit" :depends-on ("vug/ugen-base"))
+     (:file "vug/free-hook" :depends-on ("vug/ugen-base"))
+     (:file "vug/recursion" :depends-on ("vug/ugen-base"))
      (:file "vug/delay/delay-1" :depends-on ("vug/base"))
      (:file "vug/delay/delay-2" :depends-on ("vug/base"))
      (:file "vug/with-buffer" :depends-on ("vug/ugen-base"))

src/graph.lisp
@@ -1,4 +1,4 @@
-;;; Copyright (c) 2013-2017 Tito Latini
+;;; Copyright (c) 2013-2018 Tito Latini
 ;;;
 ;;; This program is free software; you can redistribute it and/or modify
 ;;; it under the terms of the GNU General Public License as published by
@@ -428,9 +428,27 @@
         (if (group-p last) (find-last-node last) last)
         node)))
 
+(defvar *dirty-nodes* (make-array *max-number-of-nodes* :fill-pointer 0))
+(declaim (type vector *dirty-nodes*))
+
+(declaim (inline save-node))
+(defun save-node (node)
+  (vector-push node *dirty-nodes*))
+
+(declaim (inline restore-node))
+(defun restore-node ()
+  (vector-pop *dirty-nodes*))
+
+(defun free-dirty-nodes ()
+  (declare #.*standard-optimize-settings*)
+  (let ((nodes (fill-pointer *dirty-nodes*)))
+    (when (plusp nodes)
+      (dotimes (i nodes (setf (fill-pointer *dirty-nodes*) 0))
+        (setf (node-id (reduce-warnings (aref *dirty-nodes* i))) nil)))))
+
+;;; Return the function to add a new node in the graph.
 (defun node-add-fn (item add-action target id hash name fn init-args
                     fade-time fade-curve)
-  "Returns the function to add a new node in the graph."
   (declare #.*standard-optimize-settings*
            (type node item target) (type symbol name add-action)
            (type fixnum hash) (type non-negative-fixnum id) (type function fn))
@@ -448,11 +466,19 @@
                (swap-nodes target new-node)
                item))))
         ((null-node-p item)
+         (setf (node-hash item) hash)
+         (setf (node-id item) id)
+         ;; It is possible to recursively call NODE-ADD-FN, so we save the node
+         ;; to avoid nested UNWIND-PROTECT's. If there is an error, the function
+         ;; FREE-DIRTY-NODES frees all the saved nodes.
+         (save-node item)
+         (if (> id 65535)
+             (setf *last-large-node-id* id)
+             (setf *last-node-id* id))
          (multiple-value-bind (init-fn perf-fn) (funcall fn item)
            (declare (type function init-fn perf-fn))
-           (setf (node-id item) id
-                 (node-hash item) hash
-                 (node-name item) name
+           (restore-node)
+           (setf (node-name item) name
                  (smp-ref (node-start-time-ptr item) 0) (now)
                  (node-pause-p item) nil
                  (node-init-function item) init-fn
@@ -466,9 +492,6 @@
                (setf (node-funcons item) (list perf-fn))
                (setf (car (node-funcons item)) perf-fn
                      (cdr (node-funcons item)) nil))
-           (if (> id 65535)
-               (setf *last-large-node-id* id)
-               (setf *last-node-id* id))
            (case add-action
              (:head
               (lambda ()

src/nrt.lisp
@@ -1,4 +1,4 @@
-;;; Copyright (c) 2013-2016 Tito Latini
+;;; Copyright (c) 2013-2018 Tito Latini
 ;;;
 ;;; This program is free software; you can redistribute it and/or modify
 ;;; it under the terms of the GNU General Public License as published by
@@ -39,6 +39,9 @@ when the duration is undefined.")
 
 (defvar *nrt-node-hash* (make-node-hash *max-number-of-nodes*))
 (declaim (type int-hash-table *nrt-node-hash*))
+
+(defvar *nrt-dirty-nodes* (make-array *max-number-of-nodes* :fill-pointer 0))
+(declaim (type vector *nrt-dirty-nodes*))
 
 (defvar *%nrt-input-pointer* (alloc-bus-pointer 'input))
 (declaim (type foreign-pointer *%nrt-input-pointer*))
@@ -139,6 +142,7 @@ when the duration is undefined.")
      (write-snd-buffer ,data ,count ,channels)
      (when (= ,count ,bufsize)
        (write-sample ,snd ,data ,bufsize)
+       (free-dirty-nodes)
        (setf ,count 0))
      (clear-outputs)
      (incf-sample-counter)))
@@ -168,12 +172,12 @@ when the duration is undefined.")
   (foreign-zero-sample *%nrt-output-pointer* *number-of-output-bus-channels*)
   (foreign-zero-sample *nrt-bus-pointer* *number-of-bus-channels*))
 
-(declaim (inline nrt-cleanup))
 (defun nrt-cleanup ()
   (flush-all-fifos)
   ;; Flush the EDF.
   (flush-pending)
   (node-free *node-root*)
+  (free-dirty-nodes)
   (incudine.edf:sched-loop))
 
 (defun write-sf-metadata-plist (sf plist)
@@ -237,6 +241,7 @@ when the duration is undefined.")
            (*allow-rt-memory-pool-p* nil)
            (*node-hash* *nrt-node-hash*)
            (*node-root* *nrt-node-root*)
+           (*dirty-nodes* *nrt-dirty-nodes*)
            (*bus-pointer* *nrt-bus-pointer*)
            (*output-pointer* *nrt-output-pointer*)
            (*input-pointer* *nrt-input-pointer*)

src/packages.lisp
@@ -410,7 +410,7 @@
    #:all-random-distributions #:rand-args
    ;; node
    #:node #:node-p #:group #:group-p #:make-group #:node-name #:node-id
-   #:node-start-time #:node-uptime
+   #:node-start-time #:node-uptime #:null-node-p
    #:next-node-id #:*node-root* #:node-release-phase-p #:node-gain
    #:node-enable-gain-p #:*node-enable-gain-p* #:node-fade-time
    #:node-fade-curve #:*fade-time* #:node-fade-in #:node-fade-out

src/rt.lisp
@@ -1,4 +1,4 @@
-;;; Copyright (c) 2013-2017 Tito Latini
+;;; Copyright (c) 2013-2018 Tito Latini
 ;;;
 ;;; This program is free software; you can redistribute it and/or modify
 ;;; it under the terms of the GNU General Public License as published by
@@ -210,6 +210,7 @@
                                 (sample (the (unsigned-byte 32) (pt:time)))))))
                   #+jack-midi (jackmidi::process ,frames)
                   (fifo-perform-functions *to-engine-fifo*)
+                  (free-dirty-nodes)
                   (do ((i 0 (+ i ,block-size)))
                       ((>= i ,frames))
                     (declare (type non-negative-fixnum i))

src/vug/ugen.lisp
@@ -1,4 +1,4 @@
-;;; Copyright (c) 2014-2017 Tito Latini
+;;; Copyright (c) 2014-2018 Tito Latini
 ;;;
 ;;; This program is free software; you can redistribute it and/or modify
 ;;; it under the terms of the GNU General Public License as published by
@@ -276,7 +276,7 @@ to call to get the control value."
             (*variables-to-preserve* nil)
             (*ugen-return-value* nil)
             (*initialization-code* (make-initialization-code-stack))
-            (,vug-body (format-vug-code ,(dsp-vug-block arguments obj)))
+            (,vug-body (format-vug-code ,(dsp-vug-block nil arguments obj)))
             (,smpvec-size (add-sample-variables))
             (,f32vec-size (add-float-variables))
             (,f64vec-size (add-double-variables))
@@ -312,7 +312,7 @@ to call to get the control value."
                         (declare (type (or incudine:node null) %dsp-node%))
                         ,@(initialization-code)
                         (,(or ',ugen-instance-constructor 'make-ugen-instance)
-                          :name ,',name
+                          :name ',',name
                           :return-pointer ,(store-ugen-return-pointer)
                           :controls ,(set-parameters-form ,ugen)
                           :init-function
@@ -400,7 +400,7 @@ to call to get the control value."
   (with-gensyms (get-function fn node)
     `(macrolet ((,get-function (,@arg-names ,node)
                   `(prog1
-                     ,(generate-ugen-code ',name ,args ,arg-names
+                     ,(generate-ugen-code ,name ,args ,arg-names
                         ,bindings ,ugen ,node ,ugen-instance-constructor
                         (maybe-store-return-value
                           ,return-type (vug-funcall ',name ,@arg-names)))

src/vug/vug.lisp
@@ -1648,14 +1648,26 @@ It is typically used to get the local variables for LOCAL-VUG-FUNCTIONS-VARS.")
        (loop for var being the hash-keys in #1#
              collect (vug-object-name var))))
 
-(defmacro vug-block (&body body)
+(defmacro with-dsp-name (name &body body)
+  (with-gensyms (fun)
+    `(flet ((,fun () (progn ,@body)))
+       (if (and ,name (not (fboundp ,name)))
+           (unwind-protect
+                (progn
+                  ;; Dummy function binding to allow recursive DSP's.
+                  (setf (symbol-function ,name) (constantly nil))
+                  (,fun))
+             (fmakunbound ,name))
+           (,fun)))))
+
+(defmacro vug-block (name &body body)
   `(mark-vug-block
      (update-vug-variables
        (fix-sequence-of-forms
          (remove-wrapped-parens
            (remove-lisp-declaration
              (list ,@(let ((*inlined-ugens* nil))
-                       (parse-vug-def body)))))))))
+                       (with-dsp-name name (parse-vug-def body))))))))))
 
 (declaim (inline fix-sequence-of-forms))
 (defun fix-sequence-of-forms (obj)
@@ -1760,7 +1772,7 @@ It is typically used to get the local variables for LOCAL-VUG-FUNCTIONS-VARS.")
                                  ,@(and doc `(,doc))
                                  (flet ((,fn ,args
                                           (with-coerce-arguments ,lambda-list
-                                            (vug-block
+                                            (vug-block nil
                                               (with-argument-bindings
                                                   (,args ,types t)
                                                 ,@vug-body)))))

commit fdc6b74504794fb8045aa2ab33ce6e915f7e38f8

Fix bug introduced in e57a3b49.

src/vug/codegen.lisp
@@ -1276,7 +1276,7 @@
   (let ((doc (if (stringp (car body)) (car body))))
     `(lambda ,arg-names
        (let ,(dsp-coercing-arguments args)
-         (,codegen-fname ',name ,args ,arg-names ,@rest
+         (,codegen-fname ,name ,args ,arg-names ,@rest
                          (progn ,@(if doc (cdr body) body)))))))
 
 (defmacro %%codegen-debug (name args defaults codegen-fname rest &body body)