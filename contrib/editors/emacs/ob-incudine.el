a2bc072efeef623a93cb0586fc7c024974dae1fb
Tito Latini <tito.01beta@gmail.com>
2018-11-09 19:00:50 +0100

    emacs mode: collection of code blocks for incudine-rego-mode
    Added two named code blocks: bounce-to-disk and bounce-to-snd.

--- contrib/editors/emacs/ob-incudine.el
+++ contrib/editors/emacs/ob-incudine.el
@@ -6,4 +6,11 @@
 
 (defalias 'org-babel-execute:incudine 'org-babel-execute:lisp)
 
+(defun load-incudine-rego-library ()
+  "Load the collection of code blocks for incudine-rego-mode."
+  (interactive)
+  (org-babel-lob-ingest (locate-library "incudine-rego-library.org")))
+
+(load-incudine-rego-library)
+
 (provide 'ob-incudine)
