d250e91e6c7e56c248b9daa2ebdeeec7d43af29e
Tito Latini <tito.01beta@gmail.com>
2020-04-04 17:29:16 +0200

    "find" command with explicit path.
    Optional path doesn't work for all the implementations.

--- src/clean.sh
+++ src/clean.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 rm -f *.o libincudine* cygincudine* barrier.h cache.lisp incudine analysis/maybe-fftw-no-simd.lisp
-find -name \*~ -print | xargs rm -f
+find . -name \*~ -print | xargs rm -f
