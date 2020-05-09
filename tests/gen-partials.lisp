titola/incudine

commit 01efa2b426a9429fe7d7e14cf856a38ff845228a

Optimize GEN:PARTIALS.

src/gen/partials.lisp
@@ -18,9 +18,9 @@
 
 (declaim (inline partial-ref))
 (defun partial-ref (partial-number amp phase dc index size tmp-var)
-  (declare (type non-negative-fixnum index size)
-           (type foreign-pointer tmp-var ))
-  (setf (smp-ref tmp-var 0)
+  (declare (type fixnum partial-number index size)
+           (type real amp phase dc))
+  (setf (smp-ref tmp-var)
         (+ dc (* amp (sin (the limited-sample
                             (+ (/ (* +twopi+ index partial-number) size)
                                (* +twopi+ phase))))))))
@@ -90,22 +90,20 @@ NORMALIZE-P to specify whether the normalization is necessary."
       (let ((size (if periodic-p size (1- size))))
         (declare (type non-negative-fixnum size)
                  #.*standard-optimize-settings* #.*reduce-warnings*)
-        (with-foreign-array (tmp 'sample)
-          (with-samples (value abs-value (max 0.0))
+        (with-samples (abs-value (max 0.0))
+          (with-foreign-array (tmp 'sample)
             (dotimes (i size)
-              (setf (smp-ref foreign-pointer i)
-                    (reduce #'+
-                      (mapcar (lambda (x)
-                                (destructuring-bind (num amp phs dc) x
-                                  (partial-ref num amp phs dc i size tmp)))
-                              pl)))
+              (setf (smp-ref foreign-pointer i) +sample-zero+)
+              (loop for (num amp phs dc) in pl do
+                      (incf (smp-ref foreign-pointer i)
+                            (partial-ref num amp phs dc i size tmp)))
               (setf abs-value (abs (smp-ref foreign-pointer i)))
-              (when (> abs-value max) (setf max abs-value)))
-            (unless periodic-p
-              (setf (smp-ref foreign-pointer size) (smp-ref foreign-pointer 0)))
-            (values foreign-pointer
-                    (if (zerop max) max (/ max))
-                    normalize-p)))))))
+              (when (> abs-value max) (setf max abs-value))))
+          (unless periodic-p
+            (setf (smp-ref foreign-pointer size) (smp-ref foreign-pointer 0)))
+          (values foreign-pointer
+                  (if (zerop max) max (/ max))
+                  normalize-p))))))
 
 ;;; BUZZ and GBUZZ are inspired by GEN11 of Csound.
 (defun buzz (num-harm)

commit 4da5a50a19535ad3b309746535de18beba183c14

Fix GEN:PARTIALS with non-harmonic partials.
Bug introduced in 01efa2b426a9429fe7d7e14cf856a38ff845228a.

doc/html/tutorial_02.html
@@ -441,8 +441,9 @@ T</pre>
 
         <li>
           <code>(partial-number strength)</code> &#x2013; <code>STRENGTH</code> is
-          the strength of the partial <code>PARTIAL-NUMBER</code>. A negative
-          <code>STRENGTH</code> value implies a phase inversion.
+          the strength of the partial <code>PARTIAL-NUMBER</code> (not necessarily
+          an integer value). A negative <code>STRENGTH</code> value implies a phase
+          inversion.
         </li>
 
         <li>

doc/tutorials/getting_start_2.cudo
@@ -339,7 +339,8 @@ waveform-1
 ;;     .5, .2 and .1
 
 ;;   * (partial-number strength) – STRENGTH is the strength of the partial
-;;     PARTIAL-NUMBER. A negative STRENGTH value implies a phase inversion.
+;;     PARTIAL-NUMBER (not necessarily an integer value). A negative
+;;     STRENGTH value implies a phase inversion.
 
 ;;   * (partial-number strength phase) – PHASE is the initial phase of the
 ;;     partial. It is a multiplier for +TWOPI+

src/gen/partials.lisp
@@ -18,8 +18,8 @@
 
 (declaim (inline partial-ref))
 (defun partial-ref (partial-number amp phase dc index size tmp-var)
-  (declare (type fixnum partial-number index size)
-           (type real amp phase dc))
+  (declare (type fixnum index size)
+           (type real partial-number amp phase dc))
   (setf (smp-ref tmp-var)
         (+ dc (* amp (sin (the limited-sample
                             (+ (/ (* +twopi+ index partial-number) size)
@@ -43,14 +43,14 @@
                                  (phase +sample-zero+ phase-p)
                                  (os +sample-zero+))
                 par
-              (declare (type positive-fixnum n) (type real amp phase os))
+              (declare (type (real 0) n) (type real amp phase os))
               (push (list n (abs amp)
                           (if (or (>= amp 0) phase-p)
                               phase
                               (sample .5))
                           os)
                     acc)
-              (setf i (floor (car par))))
+              (setf i (floor n)))
           (error ()
             (error 'incudine:incudine-memory-fault-error
                    :format-control "Malformed list of partials:~%~A in ~A"
@@ -75,7 +75,8 @@ is one of the following:
       ;; => T
 
 - list (partial-number strength): STRENGTH is the strength of the partial
-  PARTIAL-NUMBER. A negative STRENGTH value implies a phase inversion.
+  PARTIAL-NUMBER (not necessarily an integer value). A negative STRENGTH
+  value implies a phase inversion.
 
 - list (partial-number strength phase): PHASE is the initial phase of the
   partial. It is a multiplier for +TWOPI+.

tests/gen-partials.lisp
@@ -62,6 +62,19 @@
                (partest '(1000 (2 1000 .5) 1000 (4 1000 .5) (7 1000 .123))))))
   t)
 
+(deftest gen-partials-non-harmonic.1
+    (gen-partials-test-1 '((.5 100)))
+  (0 4 9 14 19 24 29 33 38 42 47 51 55 59 63 67 70 74 77 80 83 85
+   88 90 92 94 95 97 98 98 99 99 100 99 99 98 98 97 95 94 92 90 88
+   85 83 80 77 74 70 67 63 59 55 51 47 42 38 33 29 24 19 14 9 4))
+
+(deftest gen-partials-non-harmonic.2
+    (gen-partials-test-1 '((1.5 100) (7.5 -50)))
+  (0 -19 -21 2 45 92 125 130 111 80 55 51 70 101 126 130 106 62 15
+   -16 -23 -8 13 22 7 -31 -78 -117 -133 -121 -91 -62 -50 -62 -91
+   -121 -133 -117 -78 -31 7 22 13 -8 -23 -16 15 62 106 130 126 101
+   70 51 55 80 111 130 125 92 45 2 -21 -19))
+
 (defun quasi-sawtooth-test (partials)
   (loop for i from 1 to partials
         with s = #(1 1 -1 -1)