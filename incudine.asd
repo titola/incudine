titola/incudine

commit 591f1e6eacbafb4b0dcf73b956b64d8618913123

Prepare the OSC message but don’t send it if there are no values.
Useful with OSC:SIMPLE-BUNDLE within a function, for example:

    (defun osc-test (stream seconds values)
      (loop for value in values
            for i from 0 do
           (setf (osc:value stream i) value))
      (osc:send-bundle stream seconds))

    (defun osc-test-3 (stream)
      (osc:simple-bundle stream 0 "/osc/test" "iii")
      (osc-test stream 0 '(1 2 3))
      (osc-test stream 1 '(4 5 6))
      (osc-test stream 2 '(7 8 9))
      stream)

src/network/osc.lisp
@@ -1066,16 +1066,34 @@ then index the required values."
        (start-message ,s ,address ,types)
        ,@(loop for val in values for i from 0
                collect `(set-value ,s ,i ,val))
-       (send ,s))))
+       ,(if values `(send ,s) 0))))
 
 (defmacro simple-bundle (stream seconds address types &rest values)
   "Send an OSC message with timestamp SECONDS plus stream latency,
 OSC ADDRESS, OSC TYPES and arbitrary VALUES.
 
+If there are no VALUES, prepare the OSC message but don't send it.
+
 The OSC timestamp SECONDS is used with dual meaning: if it is greater
 than 63103 seconds (about 17 hours), the time is absolute otherwise it
 is added to the current time. 63104 is the offset of the NTP Timestamp
-Era 1 (from 8 Feb 2036), so this hack will work for centuries."
+Era 1 (from 8 Feb 2036), so this hack will work for centuries.
+
+Example:
+
+    (osc:simple-bundle stream .5 \"/osc/test\" \"iii\" 1 2 3)
+    ;; => 52
+
+is equivalent to
+
+    (osc:simple-bundle stream 0 \"/osc/test\" \"iii\")
+    ;; => 0
+
+    (setf (osc:value stream 0) 1)
+    (setf (osc:value stream 1) 2)
+    (setf (osc:value stream 2) 3)
+    (osc:send-bundle stream .5)
+    ;; => 52"
   (with-gensyms (s)
     `(let ((,s ,stream))
        (start-message ,s ,address ,types)
@@ -1087,7 +1105,7 @@ Era 1 (from 8 Feb 2036), so this hack will work for centuries."
          (setf (cffi:mem-ref (stream-buffer-pointer ,s) :uint32)
                (swap-bytes:htonl (stream-bundle-length ,s))))
        (set-bundle-first-element-length ,s)
-       (send-bundle ,s ,seconds))))
+       ,(if values `(send-bundle ,s ,seconds) 0))))
 
 (declaim (inline address-pattern))
 (defun address-pattern (stream &optional typetag-p)

doc/html/tutorial_osc.html
@@ -228,6 +228,20 @@
 3.704630902676296d9</pre>
 
       <p>
+        If <code>OSC:SIMPLE-BUNDLE</code> is called with no values, it prepares
+        the OSC message but doesn't send it. The previous example is equivalent to
+      </p>
+
+      <pre class="src src-lisp"><span class="prompt">SCRATCH&gt;</span> (osc:simple-bundle *oscout* 0 <span style="color: #ffa07a;">"/osc/test"</span> <span style="color: #ffa07a;">"iii"</span>)
+0
+
+<span class="prompt">SCRATCH&gt;</span> (setf (osc:value *oscout* 0) 1)
+<span class="prompt">SCRATCH&gt;</span> (setf (osc:value *oscout* 1) 2)
+<span class="prompt">SCRATCH&gt;</span> (setf (osc:value *oscout* 2) 3)
+<span class="prompt">SCRATCH&gt;</span> (osc:send-bundle *oscout* .5)
+52</pre>
+
+      <p>
         We don't send OSC bundles in the past, therefore we can use the time with
         dual meaning: if it is greater than 63103 seconds (about 17 hours), the
         time is absolute otherwise it is added to the current time.

doc/tutorials/osc_messages.cudo
@@ -173,6 +173,18 @@ osc:*max-values*
 (timestamp)
 ;; 3.704630902676296d9
 
+;; If OSC:SIMPLE-BUNDLE is called with no values, it prepares the OSC
+;; message but doesn't send it. The previous example is equivalent to
+
+(osc:simple-bundle *oscout* 0 "/osc/test" "iii")
+;; => 0
+
+(setf (osc:value *oscout* 0) 1)
+(setf (osc:value *oscout* 1) 2)
+(setf (osc:value *oscout* 2) 3)
+(osc:send-bundle *oscout* .5)
+;; => 52
+
 ;; We don't send OSC bundles in the past, therefore we can use the time
 ;; with dual meaning: if it is greater than 63103 seconds (about 17 hours),
 ;; the time is absolute otherwise it is added to the current time.

commit 50e6e112ff7feb2b5b555f854c157e909732463d

Fix OSC messages without types.
Bug introduced in 591f1e6eacbafb4b0dcf73b956b64d8618913123.

It also blocks RECV-STOP for INCUDINE.OSC:STREAM because

    (incudine.osc:message tmp "/receiver/quit" "")

fails and the receiver-thread remains alive.

incudine.asd
@@ -38,7 +38,7 @@
     (perform 'compile-op c)))
 
 (defsystem "incudine"
-  :version "0.9.32"
+  :version "0.9.33"
   :description "Incudine is a Music/DSP programming environment."
   :licence "GPL v2"
   :author "Tito Latini"

src/config.lisp
@@ -18,7 +18,7 @@
 
 (define-constant +incudine-major+ 0)
 (define-constant +incudine-minor+ 9)
-(define-constant +incudine-patch+ 32)
+(define-constant +incudine-patch+ 33)
 
 (in-package :incudine.util)
 

src/network/osc.lisp
@@ -1066,13 +1066,14 @@ then index the required values."
        (start-message ,s ,address ,types)
        ,@(loop for val in values for i from 0
                collect `(set-value ,s ,i ,val))
-       ,(if values `(send ,s) 0))))
+       ,(if (or values (string= types "")) `(send ,s) 0))))
 
 (defmacro simple-bundle (stream seconds address types &rest values)
   "Send an OSC message with timestamp SECONDS plus stream latency,
 OSC ADDRESS, OSC TYPES and arbitrary VALUES.
 
-If there are no VALUES, prepare the OSC message but don't send it.
+If TYPES is not \"\" and there are no VALUES, prepare the OSC message
+but don't send it.
 
 The OSC timestamp SECONDS is used with dual meaning: if it is greater
 than 63103 seconds (about 17 hours), the time is absolute otherwise it
@@ -1105,7 +1106,9 @@ is equivalent to
          (setf (cffi:mem-ref (stream-buffer-pointer ,s) :uint32)
                (swap-bytes:htonl (stream-bundle-length ,s))))
        (set-bundle-first-element-length ,s)
-       ,(if values `(send-bundle ,s ,seconds) 0))))
+       ,(if (or values (string= types ""))
+            `(send-bundle ,s ,seconds)
+            0))))
 
 (declaim (inline address-pattern))
 (defun address-pattern (stream &optional typetag-p)