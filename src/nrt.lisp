titola/incudine

commit 474dd14b3bf505101b446d35760e94063da44c45

Improve BOUNCE-TO-DISK

src/nrt.lisp
@@ -378,9 +378,10 @@ BPM is the tempo in beats per minute and defaults to *DEFAULT-BPM*."
         incudine::*sndfile-buffer-size*
         (- (+ incudine::*sndfile-buffer-size* channels) x))))
 
-(defun %bounce-to-disk (output-filename duration channels sample-rate
-                        header-type data-format metadata function)
-  (declare (type (or string pathname) output-filename) (type function function)
+(defun %bounce-to-disk (input-filename output-filename duration channels
+                        sample-rate header-type data-format metadata function)
+  (declare (ignore input-filename)
+           (type (or string pathname) output-filename) (type function function)
            (type channel-number channels) (type real duration))
   (with-nrt (channels sample-rate)
     (let* (;; If DURATION is negative or zero, the rendering terminates
@@ -600,18 +601,23 @@ METADATA is a property list to set string metadata in OUTPUT-FILENAME.
 Not all file types support metadata. The valid properties are: title,
 copyright, software, artist, comment, date, album, license, tracknumber
 and genre."
-  `(,@(if input-filename
-          `(%bounce-to-disk-with-infile
-             (soundfile-truename ,input-filename :input-p t))
-          '(%bounce-to-disk))
-      (soundfile-truename ,output-filename)
-      ,(or duration `(- ,pad))
-      ,channels
-      ,sample-rate
-      ,(or header-type '*default-header-type*)
-      ,(or data-format '*default-data-format*)
-      ,metadata
-      (bounce-function ,body)))
+  (with-gensyms (fname infile)
+    `(let ((,infile ,input-filename))
+       (multiple-value-bind (,fname ,infile)
+           (if ,infile
+               (values #'%bounce-to-disk-with-infile
+                       (soundfile-truename ,infile :input-p t))
+               (values #'%bounce-to-disk nil))
+         (funcall ,fname
+           ,infile
+           (soundfile-truename ,output-filename)
+           (or ,duration (- ,pad))
+           ,channels
+           ,sample-rate
+           (or ,header-type *default-header-type*)
+           (or ,data-format *default-data-format*)
+           ,metadata
+           (bounce-function ,body))))))
 
 (defmacro nrt-write-buffer-loop (in-data out-data in-count out-count
                                  in-channels out-channels in-size mix-p)

commit 0929126cf4b1ccaf3afe6805db8e4d9fd0fd6b8d

BOUNCE-TO-DISK could fail to set default values.
Bug introduced in 474dd14b3bf505101b446d35760e94063da44c45.

src/nrt.lisp
@@ -1,4 +1,4 @@
-;;; Copyright (c) 2013-2022 Tito Latini
+;;; Copyright (c) 2013-2023 Tito Latini
 ;;;
 ;;; This program is free software; you can redistribute it and/or modify
 ;;; it under the terms of the GNU General Public License as published by
@@ -627,8 +627,8 @@ The max number of scheduled events is the value of the configuration
 variable *NRT-EDF-HEAP-SIZE* (a power of two)."
   (with-gensyms (fname infile htype dformat)
     `(let ((,infile ,input-filename)
-           (,htype ,(or header-type '*default-header-type*))
-           (,dformat ,(or data-format '*default-data-format*)))
+           (,htype (or ,header-type *default-header-type*))
+           (,dformat (or ,data-format *default-data-format*)))
        (soundfile::check-format ,htype ,dformat)
        (multiple-value-bind (,fname ,infile)
            (if ,infile
@@ -638,7 +638,7 @@ variable *NRT-EDF-HEAP-SIZE* (a power of two)."
          (funcall ,fname
            ,infile
            (soundfile-truename ,output-filename)
-           ,(or duration `(- ,pad))
+           (or ,duration (- ,pad))
            ,channels ,sample-rate ,htype ,dformat ,metadata
            (bounce-function ,body))))))
 