titola/incudine

commit 047ea3a78cadbf63e0bdab96485a56e3a5f2f64b

Rearrange the code to work on Windows.
Realtime with PortAudio and PortMidi.

Networking and Serial IO are disabled at present.

contrib/cl-portmidi/cffi-portmidi.lisp
@@ -1,4 +1,4 @@
-;;; Copyright (c) 2013-2021 Tito Latini
+;;; Copyright (c) 2013-2023 Tito Latini
 ;;;
 ;;; This library is free software; you can redistribute it and/or
 ;;; modify it under the terms of the GNU Lesser General Public
@@ -26,8 +26,15 @@
     (:cygwin "cygportmidi-0.dll")
     (t (:default "libportmidi")))
 
+  (cffi:define-foreign-library porttime
+    (:darwin "libporttime.dylib")
+    (:unix "libporttime.so")
+    (:cygwin "cygporttime-0.dll")
+    (t (:default "libporttime")))
+
   (defun load-portmidi-library ()
-    (cffi:use-foreign-library portmidi))
+    (cffi:use-foreign-library portmidi)
+    (cffi:use-foreign-library porttime))
 
   (unless (cffi:foreign-library-loaded-p 'portmidi)
     (load-portmidi-library)))

.gitignore
@@ -2,6 +2,7 @@
 *.so
 *.dylib
 *.dll
+*.exe
 *.mid
 *~
 *.o

incudine-tests.asd
@@ -2,7 +2,7 @@
 ;;;
 ;;; ASDF system definition for INCUDINE unit tests.
 ;;;
-;;; Copyright (c) 2013-2022 Tito Latini
+;;; Copyright (c) 2013-2023 Tito Latini
 ;;;
 ;;; This program is free software; you can redistribute it and/or modify
 ;;; it under the terms of the GNU General Public License as published by
@@ -46,7 +46,7 @@
      (:file "envelope" :depends-on ("util"))
      (:file "midi" :depends-on ("util"))
      (:file "midifile" :depends-on ("envelope"))
-     (:file "osc" :depends-on ("util"))
+     (:file "osc" :if-feature (:not :win32) :depends-on ("util"))
      (:file "gen-partials" :depends-on ("util"))
      (:file "gen-envelope" :depends-on ("util"))
      (:file "gen-polynomial" :depends-on ("util"))

incudine.asd
@@ -39,7 +39,7 @@
     (perform 'compile-op c)))
 
 (defsystem "incudine"
-  :version "0.9.64"
+  :version "0.9.65"
   :description "Incudine is a Music/DSP programming environment."
   :licence "GPL v2"
   :author "Tito Latini"
@@ -133,6 +133,7 @@
      (:file "envelope" :depends-on ("logger" "foreign-array"))
      (:file "graph" :depends-on ("time" "logger" "int-hash"))
      (:file "node-pool" :depends-on ("graph"))
+     (:file "receiver" :depends-on ("cl-impl"))
      (:file "rt"
       :depends-on ("fifo" "bus" "graph" #+jack-audio "jack"
                                         #+jack-midi "jackmidi"
@@ -141,17 +142,18 @@
                  (incudine-maybe-recompile-source-file c "rt")))
      (:file "nrt" :depends-on ("rt"))
      (:file "score" :depends-on ("nrt"))
-     (:file "midi" :depends-on ("edf-sched" "receiver" "tuning" #+jack-midi "jackmidi"))
      (:file "jack" :if-feature :jack-audio :depends-on ("foreign"))
      (:file "jackmidi" :if-feature :jack-midi :depends-on ("fifo" "receiver"))
      (:file "portaudio" :if-feature :portaudio :depends-on ("foreign" "logger"))
-     (:file "receiver" :depends-on ("vug/midi" "network/generic" "serial/sbcl"))
-     (:file "network/sbcl-vops" :if-feature (:and :sbcl (:or :x86 :x86-64))
-                            :depends-on ("conditions"))
-     (:file "network/cffi-osc" :depends-on ("conditions"))
-     (:file "network/osc" :depends-on ("util" "network/cffi-osc" "network/sbcl-vops"))
-     (:file "network/generic" :depends-on ("network/osc"))
-     (:file "serial/sbcl" :depends-on ("cl-impl"))
+     (:file "midi" :depends-on ("edf-sched" "vug/midi" "receiver" "tuning"
+                                #+jack-midi "jackmidi"))
+     (:file "network/sbcl-vops" :if-feature (:and :sbcl (:or :x86 :x86-64) (:not :win32))
+            :depends-on ("conditions"))
+     (:file "network/cffi-osc" :if-feature (:not :win32) :depends-on ("conditions"))
+     (:file "network/osc" :if-feature (:not :win32)
+            :depends-on ("receiver" "network/cffi-osc" "network/sbcl-vops"))
+     (:file "network/generic" :if-feature (:not :win32) :depends-on ("network/osc"))
+     (:file #-win32 "serial/sbcl" #+win32 "serial/nil" :depends-on ("receiver"))
      (:file "soundfile" :depends-on ("util"))
      (:file "midifile" :depends-on ("util" "time"))
      (:file "analysis/maybe-fftw-no-simd"

src/compile-clib.lisp
@@ -1,4 +1,4 @@
-;;; Copyright (c) 2014-2022 Tito Latini
+;;; Copyright (c) 2014-2023 Tito Latini
 ;;;
 ;;; This program is free software; you can redistribute it and/or modify
 ;;; it under the terms of the GNU General Public License as published by
@@ -101,9 +101,10 @@
 
   (defvar *c-compiler-flags*
     (concatenate 'string "-O3 -Wall"
-                 #-(or darwin cygwin) " -fPIC"
+                 #-(or darwin win32) " -fPIC"
                  (if (eq *sample-type* 'double-float)
                      " -D__INCUDINE_USE_64_BIT_SAMPLE__")
+                 #+win32 " -DWIN32"
                  #+little-endian " -DLITTLE_ENDIAN"
                  (format nil " -D__INCUDINE_~A__" *sched-policy*)
                  *c-header-file-paths*))
@@ -161,7 +162,7 @@
   (defvar *c-library-type*
     #+(and unix (not darwin)) "so"
     #+darwin "dylib"
-    #+cygwin "dll")
+    #+win32 "dll")
 
   (defvar *c-library-pathname*
     (make-pathname :name *c-library-name* :type *c-library-type*
@@ -357,7 +358,7 @@
         (flet ((to-compile-p (key)
                  (or (member key objs) (missing-c-object-p key))))
           (let ((ofiles nil)
-                (libs-dep '("pthread" "m")))
+                (libs-dep '("pthread" "m" "sndfile")))
             (terpri)
             ;; RT Audio
             (case *audio-driver*
@@ -375,7 +376,7 @@
               (push "X11" libs-dep)
               (add-c-object-to-link :mouse ofiles))
             ;; Open Sound Control
-            (add-c-object-to-link :osc ofiles)
+            #-win32 (add-c-object-to-link :osc ofiles)
             ;; Utilities
             (add-c-object-to-link :ringbuffer ofiles)
             (add-c-object-to-link :util ofiles)

src/config.lisp
@@ -18,7 +18,7 @@
 
 (define-constant +incudine-major+ 0)
 (define-constant +incudine-minor+ 9)
-(define-constant +incudine-patch+ 64)
+(define-constant +incudine-patch+ 65)
 
 (in-package :incudine.util)
 

src/features.lisp
@@ -26,6 +26,11 @@
       (symbol (symbol-value form))
       (t form)))
 
+  (defun executable-p (cmd)
+    (and (handler-case (sb-ext:run-program cmd nil :search t)
+           (error () (return-from executable-p nil)))
+         t))
+
   (defparameter *c-compiler* nil)
   (defparameter *foreign-library-directories* nil)
   (defparameter *foreign-header-file-directories* nil)
@@ -47,7 +52,7 @@
     (flet ((test-library (&optional header-file error-output)
              (zerop (third (multiple-value-list
                              (uiop:run-program
-                               (format nil "~A ~A -DINCUDINE_CONFIG_TEST_HEADER='<~A>'"
+                               (format nil "~A ~A -DINCUDINE_CONFIG_TEST_HEADER=\"<~A>\""
                                        *c-libtest-fmt* name (or header-file "stdlib.h"))
                                :ignore-error-status t :error-output error-output))))))
       (when (test-library)
@@ -106,11 +111,7 @@
     (defparameter cl-user::__make_incudine_manual__ t))
   (setf cl-user::__incudine_c_compiler__
         (flet ((check (cc)
-                 (and (zerop
-                        (third (multiple-value-list
-                                 (uiop:run-program (format nil "which ~A" cc)
-                                                   :ignore-error-status t))))
-                      cc)))
+                 (if (executable-p cc) cc)))
           (let ((cc (when *c-compiler*
                       (if (stringp *c-compiler*)
                           (or (and (string/= *c-compiler* "")
@@ -136,7 +137,7 @@
           (alexandria:flatten
             (mapcar #'cffi-mini-eval *foreign-header-file-directories*))))
   (setf *c-libtest-fmt*
-        (format nil "~A -o /dev/null~A~A ~S -l"
+        (format nil "~A~A~A ~S -l"
                 cl-user::__incudine_c_compiler__
                 cl-user::__incudine_c_header_file_paths__
                 cl-user::__incudine_c_library_paths__

src/foreign.lisp
@@ -1,4 +1,4 @@
-;;; Copyright (c) 2013-2019 Tito Latini
+;;; Copyright (c) 2013-2023 Tito Latini
 ;;;
 ;;; This program is free software; you can redistribute it and/or modify
 ;;; it under the terms of the GNU General Public License as published by
@@ -20,7 +20,8 @@
   (cffi:define-foreign-library fftw
     (:darwin #+double-samples "libfftw3.dylib" #-double-samples "libfftw3f.dylib")
     (:unix #+double-samples "libfftw3.so" #-double-samples "libfftw3f.so")
-    (:cygwin #+double-samples "cygfftw3-0.dll" #-double-samples "cygfftw3f-0.dll")
+    (:cygwin #+double-samples "cygfftw3-3.dll" #-double-samples "cygfftw3f-3.dll")
+    (:win32 #+double-samples "libfftw3-3.dll" #-double-samples "libfftw3f-3.dll")
     (t (:default #+double-samples "libfftw3" #-double-samples "libfftw3f")))
 
   (unless (find-package :gsll)
@@ -28,12 +29,14 @@
       (:darwin "libgslcblas.dylib")
       (:unix "libgslcblas.so")
       (:cygwin "cyggslcblas-0.dll")
+      (:win32 "libgslcblas-0")
       (t (:default "libgslcblas")))
 
     (cffi:define-foreign-library libgsl
       (:darwin "libgsl.dylib")
       (:unix "libgsl.so")
-      (:cygwin "cyggsl-0.dll")
+      (:cygwin "cyggsl-27.dll")
+      (:win32 "libgsl-27.dll")
       (t (:default "libgsl")))
 
     (defun load-gsl-library ()

commit 2bdebb0e731764b83918adbe49e6cd92701e92a3

Fix porttime loading reported by Orm Finnendahl.
Bug introduced in the recent 047ea3a78cadbf63e0bdab96485a56e3a5f2f64b.

The check seems unnecessary because libportmidi depends on libporttime,
for example (cffi:foreign-library-loaded-p 'porttime) returns NIL.

contrib/cl-portmidi/cffi-portmidi.lisp
@@ -32,12 +32,10 @@
     (:cygwin "cygporttime-0.dll")
     (t (:default "libporttime")))
 
-  (defun load-portmidi-library ()
-    (cffi:use-foreign-library portmidi)
-    (cffi:use-foreign-library porttime))
-
   (unless (cffi:foreign-library-loaded-p 'portmidi)
-    (load-portmidi-library)))
+    (cffi:use-foreign-library portmidi)
+    (unless (cffi:foreign-symbol-pointer "Pt_Time")
+      (cffi:use-foreign-library porttime))))
 
 (defstruct (stream (:constructor %make-stream)
                    (:copier nil))