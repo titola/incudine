9d4aea14cb0e0eddbb545af976404eee1b7b641f
Tito Latini <tito.01beta@gmail.com>
2024-02-08 17:27:35 +0100

    Robust node tree operations.

--- src/util.lisp
+++ src/util.lisp
@@ -1,4 +1,4 @@
-;;; Copyright (c) 2013-2023 Tito Latini
+;;; Copyright (c) 2013-2024 Tito Latini
 ;;;
 ;;; This program is free software; you can redistribute it and/or modify
 ;;; it under the terms of the GNU General Public License as published by
